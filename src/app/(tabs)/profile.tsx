87dfe0162cfeffde03ee1264bf0aa2a2b3e41c8a
Emmanuel <emmanueldavidson524@gmail.com>
2025-05-01 14:35:59 +0100

    Cloudinary Image Upload

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,10 +1,74 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import * as ImagePicker from "expo-image-picker"
+import Button from '~/src/components/Button';
+// import {AdvancedImage} from "cloudinary-react-native";
+// import {Cloudinary} from "@cloudinary/url-gen"
+
+//Create a Cloudinary instance and set your cloud name
+// const cloudinaryInit = new Cloudinary({
+//   cloud:{
+//     cloudName:""
+//   }
+// })
 
 export default function ProfileScreen() {
+  const [caption, set_caption] = useState("");   
+  const [image, set_image] = useState<string | null>(null);
+  const [username, set_username] = useState("");
+
+  // cloudinary image
+  // const myImage = cloudinaryInit.image("")
+  
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes:ImagePicker.MediaTypeOptions.Images,
+      allowsEditing:true,
+      aspect:[1,1],
+      quality:1,
+    })
+    // console.log(result);
+    if(!result.canceled){
+      set_image(result.assets[0].uri)
+    }
+  }
+  
   return (
-    <View>
-      <Text>profile</Text>
+    <View className='p-3 flex-1'>
+      {/* Avatar Image Picker */}
+      {
+        image 
+        ? <Image  
+            source={{
+              uri:image
+            }}
+            className='w-52 aspect-square self-center rounded-full shadow-md'
+          /> 
+        : <View className='w-52 aspect-square rounded-full self-center bg-slate-300'/>
+      }
+      <Text onPress={pickImage} className='text-blue-500 font-semibold m-5 self-center'>Change</Text>
+      {/* Form  */}
+
+      <Text className='mb-2 text-gray-500 font-semibold '>Username</Text>
+
+      {/* Content */}
+      {/* <AdvancedImage 
+        cldImg={myImage}
+        className='w-full aspect-[4/3]'
+      /> */}
+
+
+      <TextInput 
+        value={username}
+        placeholder='Username'
+        onChangeText={set_username}
+        className='border border-gray-300 p-2 rounded-md'
+      />
+      {/* Button */}
+      <View className='gap-3 mt-auto'>
+        <Button title={"Update Profile"} onPress={() => {}}/>
+        <Button title={"Sign Out"} onPress={() => {}}/>
+      </View>
     </View>
   )
 }
--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -1,22 +1,61 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
 import {Feather, Ionicons, AntDesign} from "@expo/vector-icons"
+import {AdvancedImage} from "cloudinary-react-native";
+import {transformationStringFromObject} from "@cloudinary/url-gen"
+import { cloudinaryInit } from '../lib/cloudinary';
+
 
 export default function PostListItem({post}) {
+   // cloudinary image
+   const myImage = cloudinaryInit.image(post.image)
+   const transformation = transformationStringFromObject([
+    {gravity: "face", height: 150, width: 150, crop: "thumb"},
+    // {radius: 20},
+    // {effect: "sepia"},
+    // {overlay: "cloudinary_icon"},
+    // {width: 50, crop: "scale"},
+    // {opacity: 60},
+    // {effect: "brightness:90"},
+    // {flags: "layer_apply", gravity: "south_east", x: 5, y: 5},
+    // {angle: 10},
+    // {fetch_format: "png"}
+    ]);
+    const avatar_transformation = transformationStringFromObject([
+      {gravity: "face", height: 48, width: 48, crop: "thumb"},
+      // {radius: 20},
+      // {effect: "sepia"},
+      // {overlay: "cloudinary_icon"},
+      // {width: 50, crop: "scale"},
+      // {opacity: 60},
+      // {effect: "brightness:90"},
+      // {flags: "layer_apply", gravity: "south_east", x: 5, y: 5},
+      // {angle: 10},
+      // {fetch_format: "png"}
+      ]);
+   myImage.addTransformation(transformation);
+   const avatar_img = cloudinaryInit.image(post.user.avatar_url);
+   avatar_img.addTransformation(avatar_transformation);
+
   return (
-    <View className='bg-white'>
+    <View className='bg-white'>   
       {/* Header */}
       <View className="p-3 flex-row items-center gap-2">
-        <Image 
-          source={{uri:post.user.image_url}}
-          className='w-12 aspect-[4/3] rounded-full'
+        <AdvancedImage 
+          cldImg={avatar_img}
+          className='w-12 rounded-full aspect-square'
         />
         <Text className='font-semibold'>{post.user.username}</Text>
       </View>
-      <Image 
+      {/* Content */}
+      <AdvancedImage 
+        cldImg={myImage}
+        className='w-full aspect-square'
+      />
+      {/* <Image 
         source={{uri:post.image_url}}
         className='w-full aspect-square'
-      />   
+      />    */}
       <View className='flex-row gap-3 p-3' >
         
         <AntDesign name="hearto" size={20} />
